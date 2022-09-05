dce089afcf624aa7eac90872a9005e345b624e19
Dawid Eret <94192705+dawideret@users.noreply.github.com>
2022-09-05 23:47:16 +0100

    Create lattice-paths.py

---
+++ lattice-paths.py
@@ -0,0 +1,13 @@
+Rows = []
+for i in range(20):
+    Rows.append(i)
+
+Cols = []
+for i in range(20):
+    Cols.append(i)
+
+while True:
+    for mx in range(1,21):
+        for col in Cols:
+            for row in Rows:
+
